b574bbba284c133a3f198004a60d0d9073e1caa9
German DZ <germ@ndz.com.ar>
2014-04-14 12:08:29 +0200

    Use 'combi' gem for web socket bus

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source "https://rubygems.org"
 
 gem 'docker-api'
+
+# Service bus
 gem 'faye-websocket'
+gem 'combi', github: '1uptalent/combi' #path: '../combi'
 
 group :test do
   gem 'rspec-given'
--- lib/agent.rb
+++ lib/agent.rb
@@ -1,11 +1,11 @@
-require 'faye/websocket'
-require 'eventmachine'
 require 'json'
 require 'digest/sha1'
 require 'docker'
 
 require 'commands'
 require 'responses'
+
+require 'combi'
 
 class Agent
   ## ATTR READERS
@@ -17,56 +17,21 @@ class Agent
   ## END ATTR READERS
 
   def initialize(config)
-    reset_back_off_delay!
     @config = config
-    @stop_requested = false
     init_docker
-    Signal.trap('TERM') { stop! }
-    Signal.trap('KILL') { stop! }
   end
 
   def start!
-    until stop_requested?
-      loop
-      back_off! unless stop_requested?
-    end
+    @bus = Combi::ServiceBus.for(:web_socket, remote_api: remote_api, handler: self)
+    bus.start!
   end
 
-  def stop!
-    @stop_requested = true
-    EM.stop_event_loop if EM.reactor_running?
-  end
-
-  def send(payload)
-    @ws.send payload.to_json
-  end
-
-  def log(*arguments)
-    p [Time.now, api_key, arguments]
-  end
-
-  protected
-
-  def init_docker
-    Docker.url = "unix:///var/run/docker.sock" unless ENV['DOCKER_HOST']
-    puts "Using DOCKER_URL #{Docker.url}"
-    puts "Versions: #{Docker.version.inspect}"
-    puts "Info: #{Docker.info.inspect}"
-  end
-
-  def commands
-    @commands ||= {
-      'containers' => Commands::Containers.new
-    }
-  end
-  def responses
-    @responses ||= {
-      'connection' => Responses::Connection.new(self)
-    }
-  end
-
-  def stop_requested?
-    @stop_requested
+  def on_open
+    hashed_key = Base64.encode64(Digest::SHA1.digest(api_key + api_secret))
+    credentials = { key: api_key, challenge: hashed_key}
+    payload = {action: 'connection/auth', name: agent_name, credentials: credentials}
+    log :open, payload
+    bus.send payload.to_json
   end
 
   def handle_response(data)
@@ -97,58 +62,44 @@ class Agent
         response = handler.handle(operation, data)
         response.merge! action: action, req_id: req_id
         log :message_response, response
-        send(response)
+        bus.send(response.to_json)
       else
         log :message, :no_command_handler, action, data
       end
     end
   end
 
-  def is_response?(data)
-    return data.has_key? 'success'
+  def log(*arguments)
+    p [Time.now, api_key, arguments]
   end
 
-  def loop
-    EM.run do
-      log "Connecting to #{remote_api}"
-      @ws = Faye::WebSocket::Client.new(remote_api)
-
-      @ws.on :open do |event|
-        reset_back_off_delay!
-        hashed_key = Base64.encode64(Digest::SHA1.digest(api_key + api_secret))
-        credentials = { key: api_key, challenge: hashed_key}
-        payload = {action: 'connection/auth', name: agent_name, credentials: credentials}
-        log :open, payload
-        send payload
-      end
-
-      @ws.on :message do |event|
-        data = JSON.parse(event.data)
-        log :message, event.data
-        start = Time.now
-        if is_response?(data)
-          handle_response(data)
-        else
-          handle_command(data)
-        end
-        log :event_runtime, "%0.6fms" % (Time.now - start), event.data
-      end
-
-      @ws.on :close do |event|
-        log :close, event.code, event.reason
-        @ws = nil
-        EM::stop_event_loop
-      end
-    end
+  def send(payload)
+    bus.send(payload.to_json)
   end
 
-  def reset_back_off_delay!
-    @back_off_delay = 1
+  protected
+
+  def bus
+    @bus
   end
 
-  def back_off!
-    sleep @back_off_delay
-    @back_off_delay = [@back_off_delay * 2, 300].min
+  def init_docker
+    Docker.url = "unix:///var/run/docker.sock" unless ENV['DOCKER_HOST']
+    puts "Using DOCKER_URL #{Docker.url}"
+    puts "Versions: #{Docker.version.inspect}"
+    puts "Info: #{Docker.info.inspect}"
+  end
+
+  def commands
+    @commands ||= {
+      'containers' => Commands::Containers.new
+    }
+  end
+
+  def responses
+    @responses ||= {
+      'connection' => Responses::Connection.new(self)
+    }
   end
 
 end
