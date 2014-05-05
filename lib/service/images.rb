7500a3f33f3d13fd8edcf073b097ad9efb952050
German DZ <germ@ndz.com.ar>
2014-05-05 11:04:20 +0200

    exclude uninteresting images from results

--- lib/service/images.rb
+++ lib/service/images.rb
@@ -1,11 +1,18 @@
 module Service
   module Images
+
+    GREYLIST = [
+      '<none>:<none>',
+      '^dockerizeit/(.*)$'
+    ]
+
     def actions
       [:images]
     end
 
     def index(message)
-      Docker::Image.all(all: '1').map(&:info)
+      result = Docker::Image.all(all: '1').map(&:info)
+      result.reject{|image| image['RepoTags'].map{|tag| GREYLIST.map{|banned| tag.match(banned)}.any? }.any? }
     end
 
   end
