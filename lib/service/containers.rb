ec6a1d6af961b62d46a6ab8b50099bc15a980dae
Abel Muiño <amuino@gmail.com>
2014-05-08 13:12:15 +0200

    Containers#index returns an rpc response instead of sending another message to the bus

--- lib/service/containers.rb
+++ lib/service/containers.rb
@@ -7,8 +7,8 @@ module Service
 
     def index(message)
       containers = Docker::Container.all(true)
-      $bus.request 'containers', 'info', kind: 'index', result: containers.map(&:json)
-      no_response
+      response = containers.map(&:json)
+      return response
     end
 
     def run(message)
