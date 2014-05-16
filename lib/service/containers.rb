88abe9db83e6ce664e3c687c75fe38c5c5de0bda
German DZ <germ@ndz.com.ar>
2014-05-16 10:02:37 +0200

    hide password, secret and token in containers env

--- lib/service/containers.rb
+++ lib/service/containers.rb
@@ -9,10 +9,19 @@ module Service
       '^dockerizeit/agent\:(.*)$'
     ]
 
+    HIDDEN_SECRET = "*****"
+    SECRETS_GREYLIST = [
+      'PASS',
+      'SECRET',
+      'TOKEN'
+    ]
+
     def index(message)
       containers = Docker::Container.all(true)
-      response = containers.reject{|container| GREYLIST.map{|banned| container.info['Image'].match(banned)}.any? }
-      return response.map(&:json)
+      response = filtered_containers(containers).map(&:json).map do |container|
+        anonymize_container_info(container)
+      end
+      return response
     end
 
     def run(message)
@@ -25,6 +34,23 @@ module Service
       container = Docker::Container.get(message['container_id'])
       container.stop
       container.json
+    end
+
+    private
+
+    def filtered_containers(containers)
+      containers.reject{|container| GREYLIST.map{|banned| container.info['Image'].match(banned)}.any? }
+    end
+
+    def anonymize_container_info(container)
+      container['Config']['Env'] = container['Config']['Env'].map do |env_config|
+        key, value = env_config.split('=')
+        SECRETS_GREYLIST.each do |secret_match|
+          value = HIDDEN_SECRET if key.upcase.match(secret_match)
+        end
+        "#{key}=#{value}"
+      end
+      container
     end
 
   end
